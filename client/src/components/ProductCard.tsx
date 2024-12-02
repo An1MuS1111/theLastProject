9a2d257d6d228df5f131eac29e052eabe1ef1e58
An1MuS1111 <khalidrafi1111@gmail.com>
2024-12-02 15:05:04 +0800

    added nav in product card image

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
 
 type ProductType = {
   id: number;
@@ -15,13 +16,31 @@ type ProductCardProps = {
 };
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const [hidden, setHidden] = useState(true);
+
   return (
-    <div key={product.id} className="group relative bg-yellow-200">
+    <div
+      key={product.id}
+      className="group relative bg-yellow-200"
+      onMouseEnter={() => setHidden(false)}
+      onMouseLeave={() => setHidden(true)}
+    >
       <img
         alt={product.imageAlt}
         src={product.imageSrc}
         className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
       />
+
+      <ChevronLeftIcon
+        style={{ opacity: hidden ? "0" : "100", cursor: "pointer" }}
+        className="absolute left-0 top-1/2 -translate-y-1/2 h-12 w-6 bg-yellow-300"
+      />
+
+      <ChevronRightIcon
+        style={{ opacity: hidden ? "0" : "100", cursor: "pointer" }}
+        className="absolute right-0 top-1/2 -translate-y-1/2 h-12 w-6 bg-yellow-300"
+      />
+
       <div className="mt-4 flex justify-between">
         <div>
           <h3 className="text-sm text-gray-700">
