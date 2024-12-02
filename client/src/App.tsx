38e263452155e1163b51f603775af1e35ae56a62
An1MuS1111 <khalidrafi1111@gmail.com>
2024-12-02 14:01:06 +0800

    added Signup Signin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,10 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Loader from "./common/Loader";
 import DefaultLayout from "./layout/DefaultLayout";
 import Profile from "./pages/Profile";
-import TableComponent from "./components/Table/TableComponent";
 import PageTitle from "./components/PageTitle";
 import Users from "./pages/Users/index";
+import SignUp from "./pages/Authentication/SignUp";
+import SignIn from "./pages/Authentication/SignIn";
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -40,6 +41,24 @@ function App() {
             <>
               <PageTitle title="Users" />
               <Users />
+            </>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <>
+              <PageTitle title="Sign Up" />
+              <SignUp />
+            </>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <>
+              <PageTitle title="Sign In" />
+              <SignIn />
             </>
           }
         />
