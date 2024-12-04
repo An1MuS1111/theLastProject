f30f944f6053e4b8cd79fc075151f7c8ad8ff7db
An1MuS1111 <khalidrafi1111@gmail.com>
2024-12-04 17:02:29 +0800

    need to fix the usersdict

--- client/src/pages/Users/index.tsx
+++ client/src/pages/Users/index.tsx
@@ -14,6 +14,16 @@ const Users = () => {
   const [currentPage, setCurrentPage] = useState(1); // Current page
   const [pageSize, setPageSize] = useState(10); // Items per page
 
+  const UsersDict = {
+    is_admin: "Is Admin?",
+    first_name: "First Name",
+    last_name: "Last Name",
+    email: "Email Address",
+    image: "Image",
+    id: "Id",
+    password: "Password",
+  };
+
   useEffect(() => {
     //  Fetching UsersData
     (async () => {
@@ -122,7 +132,8 @@ const Users = () => {
                   sort_direction={sortDirection}
                   sortChanged={handleSortChange}
                 >
-                  {key}
+                  {/* {key} */}
+                  {UsersDict[key]}
                 </TableHeading>
               ))}
             </tr>
