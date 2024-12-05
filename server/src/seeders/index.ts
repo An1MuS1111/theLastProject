0c9a76415589d24d4b4353b761bdda873a206975
An1MuS1111 <khalidrafi1111@gmail.com>
2024-12-05 17:00:39 +0800

    small fix on the seeder file to details what's happening

--- server/src/seeders/index.ts
+++ server/src/seeders/index.ts
@@ -6,14 +6,12 @@ const seedDatabase = async () => {
     try {
         // Sync the database with the force: true option (drops and recreates tables)
         await sequelize.sync({ force: true });
+        console.log("All tables have been dropped");
 
         // Seed the User and Product data
         await UserSeeder(); // Wait for UserSeeder to complete
         await ProductSeeder(); // Wait for ProductSeeder to complete
-
-        console.log(
-            "All tables have been dropped and recreated, and data has been seeded."
-        );
+        console.log("seeding complete");
     } catch (error) {
         console.error("Error during database seeding:", error);
     } finally {
