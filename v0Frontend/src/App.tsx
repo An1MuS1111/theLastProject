fcc8865b9c0bb7d988ffdda6347aea468292f528
Abdullah Al Khalid <khalidrafi1111@gmail.com>
2025-01-19 00:15:16 +0800

    ..

--- v0Frontend/src/App.tsx
+++ v0Frontend/src/App.tsx
@@ -7,95 +7,77 @@ function App() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
         consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
         Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-
-
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-        consequatur? Minus voluptatem ipsum non consectetur, doloremque eveniet.
-        Explicabo iure placeat, magni veniam, ab vel amet consequuntur atque
-        ipsa hic dignissimos?
-        
+        ipsa hic dignissimos? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Reiciendis, consequatur? Minus voluptatem ipsum non consectetur,
+        doloremque eveniet. Explicabo iure placeat, magni veniam, ab vel amet
+        consequuntur atque ipsa hic dignissimos? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Reiciendis, consequatur? Minus voluptatem
+        ipsum non consectetur, doloremque eveniet. Explicabo iure placeat, magni
+        veniam, ab vel amet consequuntur atque ipsa hic dignissimos? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Reiciendis, consequatur?
+        Minus voluptatem ipsum non consectetur, doloremque eveniet. Explicabo
+        iure placeat, magni veniam, ab vel amet consequuntur atque ipsa hic
+        dignissimos? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Reiciendis, consequatur? Minus voluptatem ipsum non consectetur,
+        doloremque eveniet. Explicabo iure placeat, magni veniam, ab vel amet
+        consequuntur atque ipsa hic dignissimos? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Reiciendis, consequatur? Minus voluptatem
+        ipsum non consectetur, doloremque eveniet. Explicabo iure placeat, magni
+        veniam, ab vel amet consequuntur atque ipsa hic dignissimos? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Reiciendis, consequatur?
+        Minus voluptatem ipsum non consectetur, doloremque eveniet. Explicabo
+        iure placeat, magni veniam, ab vel amet consequuntur atque ipsa hic
+        dignissimos? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Reiciendis, consequatur? Minus voluptatem ipsum non consectetur,
+        doloremque eveniet. Explicabo iure placeat, magni veniam, ab vel amet
+        consequuntur atque ipsa hic dignissimos? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Reiciendis, consequatur? Minus voluptatem
+        ipsum non consectetur, doloremque eveniet. Explicabo iure placeat, magni
+        veniam, ab vel amet consequuntur atque ipsa hic dignissimos? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Reiciendis, consequatur?
+        Minus voluptatem ipsum non consectetur, doloremque eveniet. Explicabo
+        iure placeat, magni veniam, ab vel amet consequuntur atque ipsa hic
+        dignissimos? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Reiciendis, consequatur? Minus voluptatem ipsum non consectetur,
+        doloremque eveniet. Explicabo iure placeat, magni veniam, ab vel amet
+        consequuntur atque ipsa hic dignissimos? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Reiciendis, consequatur? Minus voluptatem
+        ipsum non consectetur, doloremque eveniet. Explicabo iure placeat, magni
+        veniam, ab vel amet consequuntur atque ipsa hic dignissimos? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Reiciendis, consequatur?
+        Minus voluptatem ipsum non consectetur, doloremque eveniet. Explicabo
+        iure placeat, magni veniam, ab vel amet consequuntur atque ipsa hic
+        dignissimos? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Reiciendis, consequatur? Minus voluptatem ipsum non consectetur,
+        doloremque eveniet. Explicabo iure placeat, magni veniam, ab vel amet
+        consequuntur atque ipsa hic dignissimos? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Reiciendis, consequatur? Minus voluptatem
+        ipsum non consectetur, doloremque eveniet. Explicabo iure placeat, magni
+        veniam, ab vel amet consequuntur atque ipsa hic dignissimos? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Reiciendis, consequatur?
+        Minus voluptatem ipsum non consectetur, doloremque eveniet. Explicabo
+        iure placeat, magni veniam, ab vel amet consequuntur atque ipsa hic
+        dignissimos? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Reiciendis, consequatur? Minus voluptatem ipsum non consectetur,
+        doloremque eveniet. Explicabo iure placeat, magni veniam, ab vel amet
+        consequuntur atque ipsa hic dignissimos? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Reiciendis, consequatur? Minus voluptatem
+        ipsum non consectetur, doloremque eveniet. Explicabo iure placeat, magni
+        veniam, ab vel amet consequuntur atque ipsa hic dignissimos? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Reiciendis, consequatur?
+        Minus voluptatem ipsum non consectetur, doloremque eveniet. Explicabo
+        iure placeat, magni veniam, ab vel amet consequuntur atque ipsa hic
+        dignissimos? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Reiciendis, consequatur? Minus voluptatem ipsum non consectetur,
+        doloremque eveniet. Explicabo iure placeat, magni veniam, ab vel amet
+        consequuntur atque ipsa hic dignissimos? Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Reiciendis, consequatur? Minus voluptatem
+        ipsum non consectetur, doloremque eveniet. Explicabo iure placeat, magni
+        veniam, ab vel amet consequuntur atque ipsa hic dignissimos? Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Reiciendis, consequatur?
+        Minus voluptatem ipsum non consectetur, doloremque eveniet. Explicabo
+        iure placeat, magni veniam, ab vel amet consequuntur atque ipsa hic
+        dignissimos?
       </ProductsDashboard>
     </>
   );
